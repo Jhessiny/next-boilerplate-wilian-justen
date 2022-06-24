8a14cbe30578fcfe8194fc1f3ad054eb46681ec1
Jhessiny <jhessinymattos@gmail.com>
2022-06-23 21:46:45 -0300

    chore: config

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -1,14 +1,21 @@
 import { render, screen } from '@testing-library/react'
 
-import Main from './'
+import Main from '.'
 
-describe('', () => {
-	it('', () => {
+describe('<Main />', () => {
+	it('should render the heading', () => {
 		const { container } = render(<Main />)
+
 		expect(
-			screen.getByRole('heading', { name: /react avancado/i })
+			screen.getByRole('heading', { name: /react avançado/i })
 		).toBeInTheDocument()
 
 		expect(container.firstChild).toMatchSnapshot()
+	})
+
+	it('should render the colors correctly', () => {
+		const { container } = render(<Main />)
+
+		expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
 	})
 })
